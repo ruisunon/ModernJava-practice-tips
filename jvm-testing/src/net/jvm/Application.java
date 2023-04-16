1f4c7069ae988265d89b0bc97495949e76655131
rui sun <ruisun@outlook.com>
2023-04-16 19:58:39 -0400

    add 2 more pros

---
+++ jvm-testing/src/net/jvm/Application.java
@@ -0,0 +1,33 @@
+package net.jvm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Application {
+
+    private static Map<String, String> stringContainer = new HashMap<>();
+
+    public static void main(String[] args) {
+        System.out.println("Start of program!");
+        String stringWithPrefix = "stringWithPrefix";
+
+        // Load Java Heap with 3 M java.lang.String instances
+        for (int i = 0; i < 3000000; i++) {
+            String newString = stringWithPrefix + i;
+            stringContainer.put(newString, newString);
+        }
+        System.out.println("MAP size: " + stringContainer.size());
+
+        // Explicit GC!
+        System.gc();
+
+        // Remove 2 M out of 3 M
+        for (int i = 0; i < 2000000; i++) {
+            String newString = stringWithPrefix + i;
+            stringContainer.remove(newString);
+        }
+
+        System.out.println("MAP size: " + stringContainer.size());
+        System.out.println("End of program!");
+    }
+}
